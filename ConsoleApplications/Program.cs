0f142b38ed349ee061d3c9b023e8ca653dd9eb54
fixedNick <kkos44911@gmail.com>
2022-04-30 23:07:25 +0300

    Add project files.

---
+++ ConsoleApplications/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApplications.StdRedirections;
+
+while (true)
+{
+    ShowMenu();
+    int input;
+    while (int.TryParse(Console.ReadLine(), out input) == false)
+    {
+        Console.WriteLine("Enter correct input!");
+        ShowMenu();
+    }
+
+    if (input == 0) return;
+    else if (input == 1) RedirectStdHandle.main();
+    else continue;
+}
+
+void ShowMenu()
+{
+    Console.WriteLine("0. Exit");
+    Console.WriteLine("1. Redirect standart in/out/error handles");
+    Console.Write("Choose action: ");
+}
---
+++ ConsoleApplications/RedirectStdHandle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplications.StdRedirections
+{
+    internal class RedirectStdHandle
+    {
+        private static TextWriter BaseOutputWriter;
+        private static TextWriter BaseErrorWriter;
+
+        private static String OutputFileName = "output.txt";
+        private static String ErrorFileName = "error.txt";
+
+        static private void Write(string text, bool isError = false)
+        {
+            using (StreamWriter sw = new StreamWriter(isError ? ErrorFileName : OutputFileName, append: true))
+            {
+                Console.SetOut(sw);
+                Console.WriteLine(text);
+            }
+        }
+        static private void Write(string text) => Write(text, false);
+        static private void WriteError(string text) => Write(text, true);
+        static public void main()
+        {
+            BaseOutputWriter = Console.Out;
+            BaseErrorWriter = Console.Error;
+            Console.Write("Enter line to output: ");
+            var output = Console.ReadLine();
+            Console.Write("Enter line to error: ");
+            var error = Console.ReadLine();
+
+            Write(output ?? "nothing entered into output");
+            WriteError(error ?? "nothing entered into error");
+
+            Console.SetOut(BaseOutputWriter);
+            Console.SetError(BaseErrorWriter);
+        }
+    }
+}
